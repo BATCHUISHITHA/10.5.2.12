0538953b0c446246f1188699448bfd839fbd0269
BATCHUISHITHA <155352743+BATCHUISHITHA@users.noreply.github.com>
2024-01-16 18:41:40 +0530

    Add files via upload

--- codes/fig1.py
+++ codes/fig1.py
@@ -8,14 +8,15 @@ def constant_function(value):
 
 # Create data for a constant function with value 100
 constant_two = constant_function(100)
-x = np.linspace(0,101,100)
+x = np.arange(0, 11, 1)  # Adjust the range to include only whole numbers
 y = constant_two(x)
 
 # Plot
-plt.plot(x, y, label='Constant Function (value = 100)')
+plt.plot(x, y, label='$x_1(n)$ (value = 100)')
+plt.stem(x, y, linefmt=':', markerfmt='o', basefmt='', label='$x_2(n)$: Stem plot for constant function')
 plt.xlabel('n')
-plt.ylabel('x(n)-y(n)')
-plt.title('Difference of nth terms of X and Y')
+plt.ylabel('$x(n) - y(n)$')
+plt.title('Difference of $x(n)$ and $y(n)$')
 plt.legend()
 
 # Save the plot as a PNG
--- codes/fig2.py
+++ codes/fig2.py
@@ -1,22 +1,26 @@
 import matplotlib.pyplot as plt
 
 # Generate the sequences
-ap1 = [101 + 5 * i for i in range(10)]  
-ap2 = [1 + 5 * i for i in range(10)]    
+ap1 = [101 + 5 * i for i in range(10)]
+ap2 = [1 + 5 * i for i in range(10)]
 
 # Plot both sequences on a single graph
-plt.plot(ap1, label='101, 106, ...')
-plt.plot(ap2, label='1, 6, ...')
+plt.plot(ap1, label='$x_1(n)$: 101, 106, ...')
+plt.plot(ap2, label='$x_2(n)$: 1, 6, ...')
+
+# Add stem plot
+plt.stem(ap1, linefmt=':', markerfmt='o', basefmt=' ', label='Stem plot for $x_1(n)$')
+plt.stem(ap2, linefmt=':', markerfmt='s', basefmt=' ', label='Stem plot for $x_2(n)$')
 
 # Add labels and legend
-plt.plot(x, y, label='x(n) & y(n)')
 plt.xlabel('n')
-plt.ylabel('x(n) & y(n)')
-plt.title('x(n) & y(n)')
-plt.legend()
+plt.ylabel('Values')
+plt.title('Stem Plot for $x_1(n)$ and $x_2(n)$')
+plt.legend(title='Sequences')
 
 # Save the plot as a PNG
 plt.savefig('fig2.png')
 
 # Show the plot
 plt.show()
+
